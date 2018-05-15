366f334a6fed7a076309446f2d900b7753295c55
Max-Wilhelm Bruker <bruker@uni-mainz.de>
2018-05-15 16:30:03 +0200

    Added color in PvList

--- pv_list.h
+++ pv_list.h
@@ -4,6 +4,7 @@
 #include <QObject>
 #include <QList>
 #include <QString>
+#include <QColor>
 
 class Pv {
 private:
@@ -13,17 +14,19 @@ private:
     QString getOutputPvName;
     QString screenName;
     QString description;
+    QColor color;
 public:
-    Pv(const QString &_setValuePvName, const QString &_getValuePvName, const QString &_setOutputPvName, const QString &_getOutputPvName, const QString &_screenName, const QString &_description)
-        : setValuePvName(_setValuePvName), getValuePvName(_getValuePvName), setOutputPvName(_setOutputPvName), getOutputPvName(_getOutputPvName), screenName(_screenName), description(_description) { }
+    Pv(const QString &_setValuePvName, const QString &_getValuePvName, const QString &_setOutputPvName, const QString &_getOutputPvName, const QString &_screenName, const QString &_description, const QColor &_color)
+        : setValuePvName(_setValuePvName), getValuePvName(_getValuePvName), setOutputPvName(_setOutputPvName), getOutputPvName(_getOutputPvName), screenName(_screenName), description(_description), color(_color) { }
     Pv(const Pv &pv)
-        : setValuePvName(pv.setValuePvName), getValuePvName(pv.getValuePvName), setOutputPvName(pv.setOutputPvName), getOutputPvName(pv.getOutputPvName), screenName(pv.screenName), description(pv.description) { }
+        : setValuePvName(pv.setValuePvName), getValuePvName(pv.getValuePvName), setOutputPvName(pv.setOutputPvName), getOutputPvName(pv.getOutputPvName), screenName(pv.screenName), description(pv.description), color(pv.color) { }
     const QString &getSetValuePvName() const { return setValuePvName; }
     const QString &getGetValuePvName() const { return getValuePvName; }
     const QString &getSetOutputPvName() const { return setOutputPvName; }
     const QString &getGetOutputPvName() const { return getOutputPvName; }
     const QString &getScreenName() const { return screenName; }
     const QString &getDescription() const { return description; }
+    const QColor &getColor() const { return color; }
 };
 
 class PvList : public QObject {
